38f1ced1b5846f953adf69b2286e89b2fa8f5be4
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-06-29 16:04:27 +0900

    [Bronze V] Title: 아!, Time: 72 ms, Memory: 5352 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cs
@@ -0,0 +1,11 @@
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+
+            if(second.Length <= first.Length)
+            {
+                Console.WriteLine("go");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
