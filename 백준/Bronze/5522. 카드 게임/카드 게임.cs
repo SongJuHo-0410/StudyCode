ba31a638084ff8efff98f8be696bca8d2f5d6c2b
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-06-29 16:44:14 +0900

    [Bronze V] Title: 카드 게임, Time: 68 ms, Memory: 5492 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cs
@@ -0,0 +1,7 @@
+            int result = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                result += n;
+            }
+            Console.WriteLine(result);
