65ccc513ed2db073d60c5465a86fa630a96b4c36
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-06-29 13:39:38 +0900

    [Bronze V] Title: 사파리월드, Time: 60 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cs
@@ -0,0 +1,5 @@
+            string[] input = Console.ReadLine().Split();
+            long a = long.Parse(input[0]);
+            long b = long.Parse(input[1]);
+
+            Console.WriteLine(Math.Abs(a - b));
