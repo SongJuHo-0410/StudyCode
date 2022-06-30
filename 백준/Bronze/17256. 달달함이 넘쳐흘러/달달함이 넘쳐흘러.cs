a29a54fa9cb7d2a32e29226c784419d9b3fbc54a
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-07-01 08:57:38 +0900

    [Bronze V] Title: 달달함이 넘쳐흘러, Time: 60 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.cs
@@ -0,0 +1,6 @@
+string[] a = Console.ReadLine().Split();
+string[] c = Console.ReadLine().Split();
+
+Console.Write(int.Parse(c[0]) - int.Parse(a[2]) + " ");
+Console.Write(int.Parse(c[1]) / int.Parse(a[1]) + " ");
+Console.Write(int.Parse(c[2]) - int.Parse(a[0]));
