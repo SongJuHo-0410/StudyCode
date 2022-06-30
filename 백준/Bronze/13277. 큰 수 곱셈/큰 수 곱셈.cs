7dea2c306d2104b02dfaebfd19c8b60a7a4f6b8c
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-06-30 14:16:01 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 6436 ms, Memory: 27104 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.cs
@@ -0,0 +1,5 @@
+using System.Numerics;
+string[] input = Console.ReadLine().Split();
+BigInteger n1 = BigInteger.Parse(input[0]);
+BigInteger n2 = BigInteger.Parse(input[1]);
+Console.WriteLine(n1 * n2);
