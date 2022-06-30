7f779db1c6a54779d661fb11c9e6108d08b60643
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-06-30 14:09:59 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 68 ms, Memory: 5368 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cs
@@ -0,0 +1,13 @@
+for (int i = 0; i < 100; i++)
+{
+    string input = Console.ReadLine();
+    if(input != null)
+    {
+        Console.WriteLine(input);
+    }
+    else
+    {
+        Console.Write("");
+    }
+    
+}
