f2e11a84a513af1dfbbbf36a65c95d61f91e8a1c
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-06-30 14:19:27 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 56 ms, Memory: 5352 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cs
@@ -0,0 +1,6 @@
+string[] input1 = Console.ReadLine().Split();
+string[] input2 = Console.ReadLine().Split();
+string[] input3 = Console.ReadLine().Split();
+string[] input4 = Console.ReadLine().Split();
+
+Console.WriteLine("비와이");
