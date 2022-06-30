4a26098eaf3bfa69bceb5deeeb77a1285511cad2
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-06-30 14:03:32 +0900

    [Bronze V] Title: 꼬마 정민, Time: 72 ms, Memory: 5604 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cs
@@ -0,0 +1,7 @@
+using System.Numerics;
+
+string[] input = Console.ReadLine().Split();
+BigInteger n1 = BigInteger.Parse(input[0]);
+BigInteger n2 = BigInteger.Parse(input[1]);
+BigInteger n3 = BigInteger.Parse(input[2]);
+Console.WriteLine(n1 + n2 + n3);
