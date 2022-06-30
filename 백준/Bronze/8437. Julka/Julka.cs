3ca9dbd4eb345c0f0affd39310dfe5f544ef519f
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-06-30 09:37:58 +0900

    [Bronze V] Title: Julka, Time: 64 ms, Memory: 5600 KB -BaekjoonHub

---
+++ 백준/Bronze/8437. Julka/Julka.cs
@@ -0,0 +1,6 @@
+using System.Numerics;
+
+BigInteger num = BigInteger.Parse(Console.ReadLine());
+BigInteger com = BigInteger.Parse(Console.ReadLine());
+Console.WriteLine((num + com) / 2);
+Console.WriteLine((num - com) / 2);
