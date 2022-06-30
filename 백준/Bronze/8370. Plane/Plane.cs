003c072bd3aa150e412888aae0ea3d6908ad4097
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-06-30 09:19:45 +0900

    [Bronze V] Title: Plane, Time: 60 ms, Memory: 5460 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cs
@@ -0,0 +1,8 @@
+string[] input = Console.ReadLine().Split();
+
+int n1 = int.Parse(input[0]);
+int n2 = int.Parse(input[1]);
+int n3 = int.Parse(input[2]);
+int n4 = int.Parse(input[3]);
+
+Console.WriteLine((n1 * n2) + (n3 * n4));
