24944e6757ee9d4df6676978bf2115f7d691aa6d
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-07-04 13:21:38 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 56 ms, Memory: 5364 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.cs
@@ -0,0 +1,20 @@
+            string input = Console.ReadLine();
+
+            switch(input)
+            {
+                case "2018":
+                    Console.WriteLine("A");
+                    break;
+                case "2019":
+                    Console.WriteLine("A");
+                    break;
+                case "2020":
+                    Console.WriteLine("A");
+                    break;
+                case "2021":
+                    Console.WriteLine("A");
+                    break;
+                case "2022":
+                    Console.WriteLine("A");
+                    break;
+            }
