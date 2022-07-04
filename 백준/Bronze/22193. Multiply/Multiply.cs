3149c35ca5a900d4f7bd671788dc95cd9f6cd91b
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-07-04 09:58:31 +0900

    [Bronze V] Title: Multiply, Time: 228 ms, Memory: 8480 KB -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.cs
@@ -0,0 +1,5 @@
+using System.Numerics;   
+string[] input = Console.ReadLine().Split();
+            BigInteger n1 = BigInteger.Parse(Console.ReadLine());
+            BigInteger n2 = BigInteger.Parse(Console.ReadLine());
+            Console.WriteLine(n1 * n2);
