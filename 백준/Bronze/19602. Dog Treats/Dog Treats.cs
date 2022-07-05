0489a662b3056bce33c0cd831ed4562446953906
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-07-05 15:18:42 +0900

    [Bronze IV] Title: Dog Treats, Time: 64 ms, Memory: 5456 KB -BaekjoonHub

---
+++ 백준/Bronze/19602. Dog Treats/Dog Treats.cs
@@ -0,0 +1,6 @@
+int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            int result = (1 * n1) + (2 * n2) + (3 * n3);
+            if (result > 9) Console.WriteLine("happy");
+            else Console.WriteLine("sad");
