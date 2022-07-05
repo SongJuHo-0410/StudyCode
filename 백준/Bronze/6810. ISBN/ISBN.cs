27c5d807cacc261bea31d8729d7b69008d208417
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-07-05 10:15:54 +0900

    [Bronze IV] Title: ISBN, Time: 60 ms, Memory: 5436 KB -BaekjoonHub

---
+++ 백준/Bronze/6810. ISBN/ISBN.cs
@@ -0,0 +1,5 @@
+int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            int total = (9 * 1) + (7 * 3) + (8 * 1) + (0 * 3) + (9 * 1) + (2 * 3) + (1 * 1) + (4 * 3) + (1 * 1) + (8 * 3) + (n1 * 1) + (n2 * 3) + (n3 * 1) ;
+            Console.WriteLine("The 1-3-sum is " + total);
