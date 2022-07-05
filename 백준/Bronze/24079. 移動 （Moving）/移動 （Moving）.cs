db203b9700e321f592a2d6ac0d2cde738d5873d2
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-07-05 16:48:48 +0900

    [Bronze IV] Title: 移動 (Moving), Time: 60 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/24079. 移動 （Moving）/移動 （Moving）.cs
@@ -0,0 +1,6 @@
+int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            if (n1 + n2 - 1 < n3) Console.WriteLine(1);
+            else Console.WriteLine(0);
