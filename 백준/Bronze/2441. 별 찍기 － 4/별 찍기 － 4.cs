96e85d7c52409158e9075ffa57584e359d8a8767
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-07-07 22:54:37 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 544 ms, Memory: 6580 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cs
@@ -0,0 +1,14 @@
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < num; i++)
+{
+    for (int j = 0; j < i; j++) 
+    {
+        Console.Write(" ");
+    }
+    for (int j = num; j > i; j--)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
