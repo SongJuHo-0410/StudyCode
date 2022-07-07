f2c72cdad06579e6bd5e2bfcdc94a9fca9608b41
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-07-07 16:28:36 +0900

    [Bronze IV] Title: 팬들에게 둘러싸인 홍준, Time: 60 ms, Memory: 5360 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cs
@@ -0,0 +1,4 @@
+string input = Console.ReadLine();
+            Console.WriteLine(":fan::fan::fan:");
+            Console.WriteLine(":fan::"+ input +"::fan:");
+            Console.WriteLine(":fan::fan::fan:");
