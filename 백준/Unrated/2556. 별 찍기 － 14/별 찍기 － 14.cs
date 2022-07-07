e531bde943cb378fef612020cfda4fb9273f53a8
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-07-07 22:39:47 +0900

    [Unrated] Title: 별 찍기 - 14, Time: 484 ms, Memory: 6572 KB -BaekjoonHub

---
+++ 백준/Unrated/2556. 별 찍기 － 14/별 찍기 － 14.cs
@@ -0,0 +1,10 @@
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < num; i++)
+{
+    for (int j = 0; j < num; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
