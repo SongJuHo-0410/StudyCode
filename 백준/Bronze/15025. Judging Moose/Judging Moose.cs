a322d8df86e3db5ccf6a04a1a3a97f91e7cc96ee
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-07-08 15:11:58 +0900

    [Bronze IV] Title: Judging Moose, Time: 68 ms, Memory: 5476 KB -BaekjoonHub

--- 백준/Bronze/15025. Judging Moose/Judging Moose.cs
+++ 백준/Bronze/15025. Judging Moose/Judging Moose.cs
@@ -1,9 +1,8 @@
 string[] input = Console.ReadLine().Split();
             int n1 = int.Parse(input[0]);
             int n2 = int.Parse(input[1]);
-            //int max = Math.Max(n1, n2);
+            int max = Math.Max(n1, n2);
 
             if (n1 == 0 && n2 == 0) Console.WriteLine("Not a moose");
             else if (n1 == n2) Console.WriteLine("Even " + n1 * 2);
-            else if (n1 > n2) Console.WriteLine("Odd " + n1 * 2);
-            else Console.WriteLine("Odd " + n2 * 2);
+            else if (n1 != n2) Console.WriteLine("Odd " + max * 2);
