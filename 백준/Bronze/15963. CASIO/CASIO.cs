3149e863055034b1d1dd295f153480bcd2241d2f
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-07-08 11:07:13 +0900

    [Bronze IV] Title: CASIO, Time: 64 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cs
@@ -0,0 +1,5 @@
+string[] input = Console.ReadLine().Split();
+            long a = long.Parse(input[0]);
+            long b = long.Parse(input[1]);
+            if (a == b) Console.WriteLine(1);
+            else Console.WriteLine(0);
