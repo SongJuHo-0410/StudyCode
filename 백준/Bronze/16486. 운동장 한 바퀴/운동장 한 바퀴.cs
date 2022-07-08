89b8ad28a8418a18013d7fd56c73e0e7eed812da
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-07-08 11:19:27 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 64 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cs
@@ -0,0 +1,5 @@
+int d1 = int.Parse(Console.ReadLine());
+            int d2 = int.Parse(Console.ReadLine());
+            double pi = 3.141592;
+
+            Console.WriteLine(d1 * 2 + pi * 2 * d2);
