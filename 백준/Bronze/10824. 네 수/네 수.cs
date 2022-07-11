1ce13819799429bce53205fab7bf2382a8deb9e5
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-07-11 11:22:36 +0900

    [Bronze III] Title: 네 수, Time: 64 ms, Memory: 5488 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.cs
@@ -0,0 +1,6 @@
+string[] s = Console.ReadLine().Split();
+            string a = s[0] + s[1];
+            string b = s[2] + s[3];
+            long an = long.Parse(a);
+            long bn = long.Parse(b);
+            Console.WriteLine(an + bn);
