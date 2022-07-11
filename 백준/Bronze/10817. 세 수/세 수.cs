c3a4c1fc6daef8c968d17e8f2ad7ae058a293201
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-07-11 11:16:22 +0900

    [Bronze III] Title: 세 수, Time: 68 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.cs
@@ -0,0 +1,27 @@
+            string[] input = Console.ReadLine().Split();
+            int n1 = int.Parse(input[0]);
+            int n2 = int.Parse(input[1]);
+            int n3 = int.Parse(input[2]);
+            int max = n1;
+            int min = n1;
+            int mid = n1;
+            max = Math.Max(max, n2);
+            max = Math.Max(max, n3);
+            min = Math.Min(min, n2);
+            min = Math.Min(min, n3);
+
+            if (max == n1 && min == n2)
+                mid = n3;
+            else if (max == n1 && min == n3)
+                mid = n2;
+            else if (max == n2 && min == n1)
+                mid = n3;
+            else if (max == n2 && min == n3)
+                mid = n1;
+            else if (max == n3 && min == n1)
+                mid = n2;
+            else if (max == n3 && min == n2)
+                mid = n1;
+
+
+            Console.WriteLine(mid);
