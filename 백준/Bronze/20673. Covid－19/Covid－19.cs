07cb1a14f326effa35e7d93be9ad24687a46c876
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-07-13 16:26:39 +0900

    [Bronze IV] Title: Covid-19, Time: 64 ms, Memory: 5460 KB -BaekjoonHub

---
+++ 백준/Bronze/20673. Covid－19/Covid－19.cs
@@ -0,0 +1,8 @@
+int p = int.Parse(Console.ReadLine());
+            int q = int.Parse(Console.ReadLine());
+            if(q <= p)
+            {
+                if (p <= 50 && q <= 10) Console.WriteLine("White");
+                else if (q > 30) Console.WriteLine("Red");
+                else Console.WriteLine("Yellow");
+            }
