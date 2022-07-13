1dcafef4741cfef9d3de973083aa7d41218d7e38
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-07-13 12:17:49 +0900

    [Gold III] Title: 돌 게임 7, Time: 68 ms, Memory: 5584 KB -BaekjoonHub

---
+++ 백준/Gold/9661. 돌 게임 7/돌 게임 7.cs
@@ -0,0 +1,4 @@
+using System.Numerics;
+BigInteger n = BigInteger.Parse(Console.ReadLine());
+            if (n % 5 == 2 || n % 5 == 0 ) Console.WriteLine("CY");
+            else Console.WriteLine("SK");
