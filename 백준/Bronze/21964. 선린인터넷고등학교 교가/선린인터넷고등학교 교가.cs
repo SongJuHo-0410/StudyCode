3e6e1b3598a0da760d9808d0a696d1d2e884e055
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-07-18 16:59:34 +0900

    [Bronze III] Title: 선린인터넷고등학교 교가, Time: 64 ms, Memory: 5856 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.cs
@@ -0,0 +1,6 @@
+int n = int.Parse(Console.ReadLine());
+            string s = Console.ReadLine();
+            for (int i = s.Length - 5; i < n; i++)
+            {
+                Console.Write(s[i]);
+            }
