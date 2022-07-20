ed10d534cd23e67035a2b64f2dafe2d77e0b40ab
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-07-20 10:47:45 +0900

    [Bronze III] Title: 저작권, Time: 60 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            int a = int.Parse(s[0]);
+            int l = int.Parse(s[1]);
+            Console.WriteLine((a * l) - (a - 1));
+            
+
+        }
+    }
+}
