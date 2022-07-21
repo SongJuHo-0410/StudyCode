584bb41aa70a076012c5b9ae708213f5809640ad
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-07-21 10:00:02 +0900

    [Bronze III] Title: 네 번째 수, Time: 64 ms, Memory: 5728 KB -BaekjoonHub

--- 백준/Bronze/2997. 네 번째 수/네 번째 수.cs
+++ 백준/Bronze/2997. 네 번째 수/네 번째 수.cs
@@ -24,11 +24,11 @@ namespace FirstProgram
 
             list.Sort(); //정렬
 
-            int r1 = list[1] - list[0];
-            int r2 = list[2] - list[1];
-            if (r1 == r2) list.Add(list[2] + r1);
-            else if (2 * r1 == r2) list.Add(list[1] + r1);
-            else if (2 * r2 == r1) list.Add(list[0] + r2);
+            int r1 = list[1] - list[0]; //0번과 1번의 차
+            int r2 = list[2] - list[1]; //1번과 2번의 차
+            if (r1 == r2) list.Add(list[2] + r1); //r1과 r2의 차가 같은 경우
+            else if (2 * r1 == r2) list.Add(list[1] + r1); //r2와 r1의 차가 2배인 경우
+            else if (2 * r2 == r1) list.Add(list[0] + r2); //r1과 r2의 차가 2배인 경우
 
             Console.WriteLine(list[3]);
             
