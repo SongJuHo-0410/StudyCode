44becee30d8b4056b112be1a05d6629e4b4a2286
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-08-18 18:48:58 +0900

    [Bronze III] Title: 자전거 속도, Time: 60 ms, Memory: 5636 KB -BaekjoonHub

--- 백준/Bronze/2765. 자전거 속도/자전거 속도.cs
+++ 백준/Bronze/2765. 자전거 속도/자전거 속도.cs
@@ -1,14 +1,18 @@
-double pi = 3.1415927;
-int cnt = 0;
-while (true)
+int count = 1;
+while(true)
 {
-    cnt++;
-    double diameter, rotated, sec;
-    string[] s = Console.ReadLine().Split(" ");
-    diameter = double.Parse(s[0]);
-    rotated = double.Parse(s[1]);
-    sec = double.Parse(s[2]);
-    if (rotated == 0) break;
-    double moved = diameter / (5280.0 * 12.0) * rotated * pi;
-    Console.WriteLine("Trip #{0}: {1:0.00} {2:0.00}", cnt, moved, moved / sec * 3600);
+    string[] s = Console.ReadLine().Split();
+    if (s[1] == "0") break;
+    double inch = double.Parse(s[0]);
+    double torque = int.Parse(s[1]);
+    double second = double.Parse(s[2]);
+
+    double circle = 2 * Math.PI * (inch / 2);
+    double round = ((torque * circle) / 12);
+    double distance = round / 5280;
+
+    double speed = (distance * 3600) / second;
+
+    Console.WriteLine("Trip #{0}: {1:0.00} {2:0.00}", count, distance, speed);
+    count++;
 }
