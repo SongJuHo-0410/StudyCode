9ee912f5efcf1ec059498af1c2e294c111fbc2c8
SongJuHo-0410 <54789274+SongJuHo-0410@users.noreply.github.com>
2022-09-04 09:45:38 +0900

    [Bronze IV] Title: Rectangles, Time: 80 ms, Memory: 5536 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.cs
@@ -0,0 +1,11 @@
+int r = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < r; i++)
+{
+	for (int j = 0; j < c; j++)
+	{
+		Console.Write("*");
+	}
+	Console.WriteLine();
+}
